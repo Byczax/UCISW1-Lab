ef9c7a3e84970f64951c75cca60afd25ad60e8ff
Bartosz Matysiak <qucker135@gmail.com>
2021-10-07 11:54:22 +0200

    Delete comments

--- lab01/zadanie3/vhdl_zad3.vhd
+++ lab01/zadanie3/vhdl_zad3.vhd
@@ -1,17 +1,3 @@
--- Vhdl test bench created from schematic C:\Users\Qucker135\Documents\ISE_DS_projects\NKB_to_Aiken\NKB_to_Aiken_schematic.sch - Wed Oct 06 22:49:49 2021
---
--- Notes: 
--- 1) This testbench template has been automatically generated using types
--- std_logic and std_logic_vector for the ports of the unit under test.
--- Xilinx recommends that these types always be used for the top-level
--- I/O of a design in order to guarantee that the testbench will bind
--- correctly to the timing (post-route) simulation model.
--- 2) To use this template as your testbench, change the filename to any
--- name of your choice with the extension .vhd, and use the "Source->Add"
--- menu in Project Navigator to import the testbench. Then
--- edit the user defined section below, adding code to generate the 
--- stimulus for your design.
---
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
