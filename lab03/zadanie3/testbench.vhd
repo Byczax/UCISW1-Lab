d181e5f6e10e7b8cf1d6ad89bd9c50232154a82b
Maciej Byczko <byczqq136@gmail.com>
2021-11-11 02:02:31 +0100

    remove usless comments
    Signed-off-by: Maciej Byczko <byczqq136@gmail.com>

--- lab03/zadanie3/testbench.vhd
+++ lab03/zadanie3/testbench.vhd
@@ -1,17 +1,3 @@
--- Vhdl test bench created from schematic C:\Users\Qucker135\Documents\ISE_DS_projects\HexToBscii\scheme.sch - Sun Nov 07 20:30:18 2021
---
--- Notes: 
--- 1) This testbench template has been automatically generated using types
--- std_logic and std_logic_vector for the ports of the unit under test.
--- Xilinx recommends that these types always be used for the top-level
--- I/O of a design in order to guarantee that the testbench will bind
--- correctly to the timing (post-route) simulation model.
--- 2) To use this template as your testbench, change the filename to any
--- name of your choice with the extension .vhd, and use the "Source->Add"
--- menu in Project Navigator to import the testbench. Then
--- edit the user defined section below, adding code to generate the 
--- stimulus for your design.
---
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
@@ -56,13 +42,6 @@ BEGIN
 		WYJ6 => WYJ6, 
 		WYJ7 => WYJ7
    );
-
--- *** Test Bench - User Defined Section ***
---   tb : PROCESS
---   BEGIN
---      WAIT; -- will wait forever
---   END PROCESS;
--- *** End Test Bench - User Defined Section ***
 
 	WEJ <= "0000", "0001" after 100 ns, "0010" after 200 ns, "0011" after 300 ns, "0100" after 400 ns, "0101" after 500 ns, "0110" after 600 ns, "0111" after 700 ns, "1000" after 800 ns, "1001" after 900 ns, "1010" after 1000 ns, "1011" after 1100 ns, "1100" after 1200 ns, "1101" after 1300 ns, "1110" after 1400 ns, "1111" after 1500 ns;
 
--- lab03/zadanie4/testbench.vhd
+++ lab03/zadanie4/testbench.vhd
@@ -1,17 +1,3 @@
--- Vhdl test bench created from schematic C:\Users\Qucker135\Documents\ISE_DS_projects\ComparatorFourBitDrei\scheme.sch - Sun Nov 07 22:02:22 2021
---
--- Notes: 
--- 1) This testbench template has been automatically generated using types
--- std_logic and std_logic_vector for the ports of the unit under test.
--- Xilinx recommends that these types always be used for the top-level
--- I/O of a design in order to guarantee that the testbench will bind
--- correctly to the timing (post-route) simulation model.
--- 2) To use this template as your testbench, change the filename to any
--- name of your choice with the extension .vhd, and use the "Source->Add"
--- menu in Project Navigator to import the testbench. Then
--- edit the user defined section below, adding code to generate the 
--- stimulus for your design.
---
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
@@ -45,15 +31,7 @@ BEGIN
 		ROWNE => ROWNE
    );
 
--- *** Test Bench - User Defined Section ***
---   tb : PROCESS
---   BEGIN
---      WAIT; -- will wait forever
---   END PROCESS;
--- *** End Test Bench - User Defined Section ***
-
 	WEJA <= "0000", "0001" after 100 ns, "0010" after 200 ns, "0100" after 300 ns, "1101" after 400 ns, "1110" after 500 ns, "1111" after 600 ns;
 	WEJB <= "0000", "0000" after 100 ns, "0011" after 200 ns, "1011" after 300 ns, "0011" after 400 ns, "1110" after 500 ns, "1100" after 600 ns;
-
 
 END;
