56ceab7e96cc67233880b4723b0d3f54acef8309
Maciej Byczko <byczqq136@gmail.com>
2021-12-11 13:25:34 +0100

    Removed unnesecary comments
    Signed-off-by: Maciej Byczko <byczqq136@gmail.com>

--- lab05/zadanie1/counter_mod.vhd
+++ lab05/zadanie1/counter_mod.vhd
@@ -1,36 +1,8 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    20:18:41 12/10/2021 
--- Design Name: 
--- Module Name:    counterMod - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.std_logic_unsigned.all;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use ieee.numeric_std.all;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity counterMod is
     Port ( WEJ : in  STD_LOGIC_VECTOR (7 downto 0);
--- lab05/zadanie1/counter_mod_benchmark.vhd
+++ lab05/zadanie1/counter_mod_benchmark.vhd
@@ -1,43 +1,10 @@
---------------------------------------------------------------------------------
--- Company: 
--- Engineer:
---
--- Create Date:   20:46:03 12/10/2021
--- Design Name:   
--- Module Name:   C:/Users/Qucker135/Documents/ISE_DS_projects/OperationalCounter/counter_mod_benchmark.vhd
--- Project Name:  OperationalCounter
--- Target Device:  
--- Tool versions:  
--- Description:   
--- 
--- VHDL Test Bench Created by ISE for module: counterMod
--- 
--- Dependencies:
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
---
--- Notes: 
--- This testbench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under test.  Xilinx recommends
--- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
--- simulation model.
---------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
- 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---USE ieee.numeric_std.ALL;
  
 ENTITY counter_mod_benchmark IS
 END counter_mod_benchmark;
  
 ARCHITECTURE behavior OF counter_mod_benchmark IS 
- 
-    -- Component Declaration for the Unit Under Test (UUT)
  
     COMPONENT counterMod
     PORT(
@@ -48,7 +15,6 @@ ARCHITECTURE behavior OF counter_mod_benchmark IS
          WYJ : OUT  std_logic_vector(7 downto 0)
         );
     END COMPONENT;
-    
 
    --Inputs
    signal WEJ : std_logic_vector(7 downto 0) := (others => '0');
@@ -81,27 +47,16 @@ BEGIN
 		CLK_LF <= '1';
 		wait for CLK_LF_period/2;
    end process;
- 
 
    -- Stimulus process
    stim_proc: process
    begin		
       -- hold reset state for 100 ns.
 
-
 		WEJ <= "11111100", "11110011" after 650ns;
 		--CLK_LF <= not CLK_LF after 50ns;
 		RESET <= '1', '0' after 10ns, '1' after 650ns, '0' after 750ns;
 		REVERSE <= '0', '1' after 650ns;
-
-      --wait for 100 ns;	
-
-
-
-      --wait for CLK_LF_period*10;
-
-      -- insert stimulus here 
-
       wait;
    end process;
 
